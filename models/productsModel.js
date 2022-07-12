7c70be33b2a93a45450777fa268278fa48cef7b2
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-07-12 17:18:35 -0300

    feat: adiciona arquivos de teste

--- models/productsModel.js
+++ models/productsModel.js
@@ -2,12 +2,12 @@ const db = require('./connection');
 
 const productsModel = {
   async list() {
-    const sql = 'SELECT * FROM StoreManager.products';
-    const [items] = await db.query(sql);
-    return items;
-  },
+      const sql = 'SELECT * FROM StoreManager.products';
+      const [items] = await db.query(sql);
+      return items;
+    },
 
-    async exists(id) {
+  async exists(id) {
     const sql = 'SELECT 1 FROM StoreManager.products WHERE id = ?';
     const [[exists]] = await db.query(sql, [id]);
     return !!exists;
---
+++ tests/unit/services/productsService.test.js
@@ -0,0 +1,42 @@
+const sinon = require('sinon');
+const productsModel = require('../../../models/productsModel');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const productsService = require('../../../services/productsService');
+
+chai.use(chaiAsPromised);
+
+describe('services/productsService', () => {
+  beforeEach(sinon.restore)
+  describe('list', () => {
+    it('throws error if model throws error', () => {
+      sinon.stub(productsModel, 'list').rejects();
+      chai.expect(productsService.list()).to.eventually.be.rejected;
+    });
+
+    it('throws error if model doesnt return an array', () => {
+      sinon.stub(productsModel, 'list').resolves({});
+      chai.expect(productsService.list()).to.eventually.be.rejected;
+    });
+
+    it('returns array of objects if successful', () => {
+      const result = [
+        {
+          id: 1,
+          name: "Martelo de Thor",
+        },
+        {
+          id: 2,
+          name: "Traje de encolhimento",
+        },
+        {
+          id: 3,
+          name: "Escudo do Capitão América",
+        },
+      ]
+      sinon.stub(productsModel, 'list').resolves(result);
+      chai.expect(productsService.list()).to.eventually.equal(result);
+    });
+
+  })
+})
